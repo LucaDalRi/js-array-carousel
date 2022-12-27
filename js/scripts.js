4de4c236d8afbbe7f8f2f9a7bc2c19a5cc63dff1
LucaDalRi <euro2xd@gmail.com>
2022-12-27 22:18:38 +0100

    Sto testando ma sembra funzionare

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,40 @@
-const imgCarosello = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp', ]
+const imgCarosello = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
+const btnRight = document.getElementById("arrow-right");
+const btnLeft = document.getElementById("arrow-left");
+let currentImg = 1;
+
+console.log(currentImg);
+
+btnRight.addEventListener("click", avanti);
+function avanti() {
+    
+    imgCarosello[currentImg++]
+
+    console.log(currentImg);
+    
+    if (currentImg > 5) {
+        
+        currentImg = 1;
+        
+    }
+
+    document.getElementById("immagine").src = "img/0" + currentImg + ".webp";
+}
+
+btnLeft.addEventListener("click", indietro);
+function indietro() {     
+    
+    imgCarosello[currentImg--]
+
+    if (currentImg <= 0) {
+        
+        currentImg = 5;
+        
+    }
+
+    document.getElementById("immagine").src = "img/0" + currentImg + ".webp";
+    
+    console.log(currentImg);
+    
+} 
+    
